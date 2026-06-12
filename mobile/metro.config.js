d222142eaab12583c9bd90b9f3bdb7e718c9e53e
Mahmut Mese <39201076+Mahmut-Mese@users.noreply.github.com>
2026-06-12 14:07:18 +0100

    Enable mobile web build support

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,5 +1,31 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+config.resolver.platforms = Array.from(new Set([...(config.resolver.platforms || []), 'web']));
+
+const defaultResolveRequest = config.resolver.resolveRequest;
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (
+    platform === 'web' &&
+    moduleName === 'react-native/Libraries/Components/TextInput/TextInputState'
+  ) {
+    return {
+      filePath: path.join(__dirname, 'src/web/TextInputStateShim.js'),
+      type: 'sourceFile',
+    };
+  }
+
+  if (platform === 'web' && moduleName === 'react-native') {
+    return context.resolveRequest(context, 'react-native-web', platform);
+  }
+
+  if (defaultResolveRequest) {
+    return defaultResolveRequest(context, moduleName, platform);
+  }
+
+  return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = config;
---
+++ mobile/src/web/TextInputStateShim.js
@@ -0,0 +1,19 @@
+const TextInputState = {
+  blurInput() {},
+  blurTextInput() {},
+  currentlyFocusedField() {
+    return null;
+  },
+  currentlyFocusedInput() {
+    return null;
+  },
+  focusInput() {},
+  focusTextInput() {},
+  isTextInput() {
+    return false;
+  },
+  registerInput() {},
+  unregisterInput() {},
+};
+
+module.exports = TextInputState;
